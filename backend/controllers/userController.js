6763d9ac78238ba2e5d707d91b690cad2e0c2e0f
fancymithun <callmeprabhu7@gmail.com>
2023-07-09 11:06:28 +0530

    Updated controllers

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,1 +1,22 @@
-module.exports = userController
+
+const signin = async(req,res) => {
+
+}
+
+const signup = async(req,res) => {
+
+}
+
+const deleteUser = async(req,res) => {
+
+}
+
+const getUser = async(req,res) => {
+
+}
+
+const getAllUsers = async(req,res) => {
+
+}
+
+module.exports = {signin,signup,deleteUser,getUser,getAllUsers}
